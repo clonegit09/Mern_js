04ada4a7c43ef24d0c9011469578b689f3f57270
clonegit09 <shreyashshukla810@gmail.com>
2025-05-13 18:56:11 +0530

    Add files via upload

---
+++ MERN_JS/JS_day2/prog2.js
@@ -0,0 +1,14 @@
+// function showTable() {
+//   let n = document.getElementById("num").value;
+//   let text = "";
+//   for (let i = 1; i <= 10; i++) {
+//     text += n + " x " + i + " = " + n * i + "<br>";
+//   }
+//   document.getElementById("result").innerHTML = text;
+// }
+function xyz() {
+  return a + b;
+}
+console.log(xyz(5, 10));
+let z = xyz(9 + 10);
+console.log(z);
